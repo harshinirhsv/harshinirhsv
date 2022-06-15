9ef9d322fe7fa99ca0ec24f9ecb0c746e2c90c73
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-06-15 18:26:44 +0530

    Create Print.js

---
+++ Print.js
@@ -0,0 +1,17 @@
+//JS program to print Hello world
+
+// console.log() is used in debugging the code.
+console.log('Hello World');
+
+
+// The alert() method displays an alert box over
+ the current window with the specified message
+alert("Hello, World!");
+
+
+//document.write() is used when you want 
+to print the 
+content to the HTML document.
+document.write('Hello, World!');
+
+
