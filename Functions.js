a082516cddfb759432ae65d323c8dbfeedc495fd
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-06-15 18:51:20 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,47 @@
+//In this example, we will make a simple function and print the values.
+
+
+<script>
+
+ 
+
+    // Declare a variable and initialize it
+
+    // Gloabal variable declaration
+
+    var Name = "mango";
+
+ 
+
+    // Function definition
+
+    function MyFunction() {
+
+ 
+
+        // Local variable declaration
+
+        var num = 45;
+
+ 
+
+        // Display the value of Gloabal variable
+
+        document.writeln(Name);
+
+ 
+
+        // Display the value of local variable
+
+        document.writeln("<br>" + num);
+
+    }
+
+     
+
+    // Function call
+
+    MyFunction();
+
+</script>
+
