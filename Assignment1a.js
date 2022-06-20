af268dbfd361759ae9c4fbea7b95c5acd1f5a22e
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-06-20 12:00:54 +0530

    Create Assignment1a.js

---
+++ Assignment1a.js
@@ -0,0 +1,7 @@
+// The Number.isInteger() checks to see if the passed value is an integer
+// Returns true or false
+
+Number.isInteger(21); //True
+Number.isInteger(16); //True
+Number.isInteger("21"); //False
+Number.isInteger(true); //false
