1088ff42153df6e1f4cb772722eb96b47abb4d09
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-06-20 12:10:14 +0530

    Create Assignment 1b.js

---
+++ Assignment 1b.js
@@ -0,0 +1,24 @@
+Question:"Using the forEach function defined for an array, find the sum of the array of numbers. [function add_all(arr) {...}]” 
+
+
+ Function add_all(arr) {
+
+var arr = [4, 8, 7, 13, 12] ;
+var sum=0;
+// Calculation the sum using forEach 
+
+    arr.forEach(x => { 
+
+        sum += x; 
+}); 
+
+// Prints: 44 
+
+    document.write("Sum is " + sum); 
+
+</script>
+
+
+    }
+
+
