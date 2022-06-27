f34cbb75a5d99d890b5aa874df31fb34b5fddf4a
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-06-27 08:48:56 +0530

    Create Assignment 2.js

---
+++ Assignment 2.js
@@ -0,0 +1,21 @@
+** Problem 1 **/
+function str_reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+/** Problem 2 **/
+function circle_comparator(c1, c2) {
+  return c1.radius - c2.radius;  
+}
+// then we can use this in the sort function
+
+
+/** Problem 3 **/
+function length_of_array(arr) {
+
+  var counter = 0;
+  
+  arr.forEach(function() {counter++;});
+  
+  return counter;
+}
