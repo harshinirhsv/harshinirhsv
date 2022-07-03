82320a08e47a1a67fd41f305604c4361f16ff7fb
harshinirhsv <98758206+harshinirhsv@users.noreply.github.com>
2022-07-03 13:48:31 +0530

    Update index.js

--- first-project/index.js
+++ first-project/index.js
@@ -1,1 +1,9 @@
+const express =require('express')
+const res = require('express/lib/response')
+const app =express()
+app.get('/', (req,res,next) => {
+    res.send('Hello!!')
+})
+
+app.listen(3000) //5000,3000,8000,8080
 
